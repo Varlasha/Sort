98d31abf05a6b2237a4eb4970aba06b50ac6de5d
Varlasha <95291773+Varlasha@users.noreply.github.com>
2022-03-18 20:20:13 +0300

    Create Main(1).java

---
+++ Main(1).java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    		 try (PrintWriter out = new PrintWriter("output.txt")) {
+	 	List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+	    			 Collections.sort(lines);
+	    		        for(String str: lines) {
+	    		        	out.println(str.replaceAll("[0-9]", ""));
+	    		        }        
+	  }
+	}
+ 
+}
